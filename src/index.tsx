5d7feb93539dc384128f543b8515d18eea300dff
Reginaldo Morais <reginaldo.cmorais@gmail.com>
2020-07-04 21:38:24 -0300

    feat: Exports Choice components

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import If from './if/If';
 import Switch from './switch/Switch';
 import Case from './switch/Case';
+import Choice from './choice/Choice';
+import InCaseOf from './choice/InCaseOf';
+import Otherwise from './choice/Otherwise';
 
-export { If, Switch, Case };
+export { If, Switch, Case, Choice, InCaseOf, Otherwise };
