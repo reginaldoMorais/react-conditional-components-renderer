93509d96803a9dd98dc6fb12c503b7e412a9498b
Reginaldo Morais <reginaldo.morais@americanas.io>
2023-03-28 14:31:59 -0300

    chore: adds more if component types

--- src/if/__test__/If.test.tsx
+++ src/if/__test__/If.test.tsx
@@ -10,8 +10,34 @@ describe("<If />", () => {
     expect(elementText).toBeTruthy();
   });
 
+  test(`should render child component if test a object not undefined`, () => {
+    const someObject = { a: 1 };
+    const { getByText }: RenderResult = render(<div><If test={someObject}><div>Show component</div></If></div>);
+    const elementText = getByText(/Show component/i);
+    expect(elementText).toBeTruthy();
+  });
+
+  test(`should render child component if test a object param not undefined`, () => {
+    const someObject = { a: 1 };
+    const { getByText }: RenderResult = render(<div><If test={someObject.a}><div>Show component</div></If></div>);
+    const elementText = getByText(/Show component/i);
+    expect(elementText).toBeTruthy();
+  });
+
   test(`should render Else component if test be false`, () => {
     const { getByText }: RenderResult = render(<div><If test={false} orElse={<div>Show other component</div>}><div>Show component</div></If></div>);
+    const elementText = getByText(/Show other component/i);
+    expect(elementText).toBeTruthy();
+  });
+
+  test(`should render Else component if test be undefined`, () => {
+    const { getByText }: RenderResult = render(<div><If test={undefined} orElse={<div>Show other component</div>}><div>Show component</div></If></div>);
+    const elementText = getByText(/Show other component/i);
+    expect(elementText).toBeTruthy();
+  });
+
+  test(`should render Else component if test be null`, () => {
+    const { getByText }: RenderResult = render(<div><If test={null} orElse={<div>Show other component</div>}><div>Show component</div></If></div>);
     const elementText = getByText(/Show other component/i);
     expect(elementText).toBeTruthy();
   });
